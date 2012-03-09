2cdf4ab5cc3b93025aa0501d228d07b7d6c02bfd
Documentopia.com <coding@documentopia.com>
2012-03-09 17:17:33 -0500

    Retrack database.php

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+
+// Put your username/password and datbase in here
+
+$db['default']['username'] = "username1";
+$db['default']['password'] = "password2";
+$db['default']['database'] = "database3";
+$db['default']['dbdriver'] = "mysqli";
+
+?>
